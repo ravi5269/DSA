e0dd53beeb8e6f93184449c5089f47940dbe054d
ravi5269 <ravipatelkant@gmail.com>
2024-07-18 19:57:38 +0530

    dayv22

---
+++ day 22/binary_tree_lavel_order_traversel.py.py
@@ -0,0 +1,52 @@
+# Binary Tree Level Order Traversal
+# Medium
+# Given the root of a binary tree, return the level order traversal
+# of its nodes' values. (i.e., from left to right, level by level).
+
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def level_order(root: TreeNode):
+    if not root:
+        return []
+
+    result = []
+    queue = deque([root])
+
+    while queue:
+        level_size = len(queue)
+        level = []
+
+        for _ in range(level_size):
+            node = queue.popleft()
+            level.append(node.val)
+            
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        
+        result.append(level)
+
+    return result
+
+
+# Constructing the binary tree:
+#        1
+#       / \
+#      2   3
+#     / \
+#    4   5
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+
+# Calling the function
+print(level_order(root))  # Output: [[1], [2, 3], [4, 5]]
